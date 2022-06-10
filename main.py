f053e26a29a698dcd552fc559285b283617846d2
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2022-06-10 14:16:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord, time
 from discord.ext import commands
 
 client = commands.Bot(command_prefix=".", case_insensitive=True)
-client.remove_command("help")
+#client.remove_command("help") haven't added custom help command yet so just keep the default one
 
 @client.event
 async def on_ready():
